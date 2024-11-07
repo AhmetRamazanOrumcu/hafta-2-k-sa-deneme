c210ca37135c5d326f45da7da618a3b1ae4c3cec
Ahmet Ramazan Örümcü <ahmetramazanmr@outlook.com>
2024-11-07 10:57:54 +0300

    Add files via upload

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+                Console.WriteLine("Türkçe Notunu Giriniz:");
+                int turkceNotu=int.Parse(Console.ReadLine());
+
+                Console.WriteLine("Matematik Notunu Giriniz:");
+                int matNotu = int.Parse(Console.ReadLine());
+            */
+            /*
+            Console.WriteLine("Notunu Giriniz:");
+            int notu;
+            if (int.TryParse(Console.ReadLine(), out notu))
+            {
+                if (notu >= 0 && notu <= 100)
+                {
+                    if (notu >= 45)
+                        Console.WriteLine("Geçti");
+                    else
+                        Console.WriteLine("kaldı");
+                }
+                else
+                {
+                    Console.WriteLine("Lütfen Geçerli bir not giriniz.");
+                }
+            }
+            else Console.WriteLine("Sayı giriniz.");
+
+            Console.ReadKey();
+            */
+
+
+
+            Console.WriteLine("Gün giriniz:");
+            string gun;
+            if(string.TryParse(ReadLine().ToLower(),out gun))
+
+            Console.ReadKey();  
+        }
+    }
+}
